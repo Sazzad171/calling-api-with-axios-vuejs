9a9d86e48ae6a7ecc0ac13cfe5a04849a6a53f11
Sazzad <sajjad.cse.diu@gmail.com>
2020-01-12 00:00:08 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="todo in todos">
+      <p> {{todo.title}} 
+        <button>X</button>
+      </p>
+    </div>
+    <addItem v-on:add-item="addItem"/>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import addItem from './components/addItem'
+import axios from 'axios'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      todos: []
+    }
+  },
+
   components: {
-    HelloWorld
+    addItem
+  },
+
+  methods: {
+    addItem(newItem) {
+      const { title, completed} = newItem;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+      .then(res => this.todos = [...this.todos, res.data])
+    }
+  },
+  
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')        //can be limit of data
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/addItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form @submit="addItem">
+      Add new todo item:
+      <input type="text" v-model="title">
+      <input type="submit" value="Add">
+    </form>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'AddItem',
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addItem(e) {
+      e.preventDefault();
+      const newItem = {
+        title: this.title,
+        completed: false
+      }
+      // send up to parent
+      this.$emit('add-item', newItem);
+      this.title='';
+    }
+  }
+}
+</script>
+
+
+<style  scoped>
+</style>
